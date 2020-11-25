4eb8abd00e93c6dda10444d39130b463d726ac89
gokulrajats <tsgr1999@gmail.com>
2020-11-25 12:41:11 +0530

    Motion Layout based Splash Screen added

--- app/src/main/java/com/example/androidinterviewprep/MainActivity.kt
+++ app/src/main/java/com/example/androidinterviewprep/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.androidinterviewprep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,5 +11,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        motionLayout?.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@MainActivity, LandingScreenActivity::class.java))
+                finish()
+            }
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) { }
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) { }
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) { }
+        })
     }
 }
