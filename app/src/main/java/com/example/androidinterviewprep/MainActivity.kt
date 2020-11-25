bce8e68f3e5d97713316e335ce66c43d61e855e8
gokulrajats <tsgr1999@gmail.com>
2020-11-25 11:05:55 +0530

    Base Branch for further works

--- app/src/main/java/com/example/androidinterviewprep/MainActivity.kt
+++ app/src/main/java/com/example/androidinterviewprep/MainActivity.kt
@@ -9,19 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val movies = listOf(
-            Movie(1, "", true, "UA", 73, 2300, "Marvel", "English", "2D"),
-            Movie(1, "", true, "UA", 73, 2300, "Marvel", "English", "2D"),
-            Movie(1, "", true, "UA", 73, 2300, "Marvel", "English", "2D"),
-            Movie(1, "", true, "UA", 73, 2300, "Marvel", "English", "2D"),
-            Movie(1, "", true, "UA", 73, 2300, "Marvel", "English", "2D"),
-            Movie(1, "", true, "UA", 73, 2300, "Marvel", "English", "2D"),
-            Movie(1, "", true, "UA", 73, 2300, "Marvel", "English", "2D"),
-            Movie(1, "", true, "UA", 73, 2300, "Marvel", "English", "2D"),
-
-
-            )
-        recylerView.layoutManager = LinearLayoutManager(this)
-        recylerView.adapter = MoviesAdapter(movies)
     }
 }
