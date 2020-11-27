8dee202f5d08920f03f5bbae064e1a378a62fa42
gokulrajats <tsgr1999@gmail.com>
2020-11-27 13:49:52 +0530

    Viewpager using FragmentPagerAdapter and Fragment Transformation

--- app/src/main/java/com/example/androidinterviewprep/MainActivity.kt
+++ app/src/main/java/com/example/androidinterviewprep/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidinterviewprep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, ScreenSlidePagerActivity::class.java)
+        startActivity(intent)
     }
 }
